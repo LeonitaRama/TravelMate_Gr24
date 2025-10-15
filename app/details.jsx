4d99ae7d943dd5da20bc9b1d556b08a9018d169a
Vlore Thaqi <116511558+Vlorethaqi@users.noreply.github.com>
2025-10-15 18:02:30 +0200

    ndryshime ne file-in details

--- app/details.jsx
+++ app/details.jsx
@@ -11,6 +11,39 @@ export default function Details(){
         <Text style={{marginTop: 10, fontSize: 16, textAlign: "center"}}>
                     Budva, a beautiful coastal city in Montenegro known for its beaches and historic old town.
 
+ </Text>
+            <Image
+          source={{ uri: "https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F80793fef-bb52-4f4e-8e33-a3c928151390.jpg?crop=5760%2C3840%2C0%2C0&resize=1770&format=webp&quality=9 "}}
+        style={{ width: 300, height: 200 }}
+        />
+        <Text style={{marginTop: 10, fontSize: 16, textAlign: "center"}}>
+                    Budva, a beautiful coastal city in Montenegro known for its beaches and historic old town.
+
+
+        </Text>
+            <Image
+          source={{ uri: "https://www.thetimes.com/imageserver/image/%2F82e05180-6d46-486e-b160-5d719a8cbd81.jpg?crop=4214%2C2371%2C278%2C920&format=webp&quality=9&resize=1500" }}
+        style={{ width: 300, height: 200 }}
+        />
+        <Text style={{marginTop: 10, fontSize: 16, textAlign: "center"}}>
+                    Budva, a beautiful coastal city in Montenegro known for its beaches and historic old town.
+
+       
+        </Text>
+            <Image
+          source={{ uri: "https://content.r9cdn.net/rimg/dimg/e2/9f/a0069cbb-city-7228-16bdc8cb53f.jpg?width=1366&height=768&xhint=2335&yhint=1945&crop=true&watermarkposition=lowerright" }}
+        style={{ width: 300, height: 200 }}
+        />
+        <Text style={{marginTop: 10, fontSize: 16, textAlign: "center"}}>
+                    Budva, a beautiful coastal city in Montenegro known for its beaches and historic old town.
+
+        </Text>
+            <Image
+          source={{ uri: "https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F4bdd2c05-3e74-47d7-8f82-b3cd8d343bd4.jpg?crop=2560%2C1706%2C0%2C0&resize=1770&format=webp&quality=9" }}
+        style={{ width: 300, height: 200 }}
+        />
+        <Text style={{marginTop: 10, fontSize: 16, textAlign: "center"}}>
+The aurora borealis lights up the sky above Ion Adventure Hotel in Iceland, creating a magical and unforgettable scene.
         </Text>
         </View>
     )
