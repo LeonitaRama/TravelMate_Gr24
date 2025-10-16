6b51a4dc2e1fd33dde34b0bc27ade20bb0c225c3
Vlore Thaqi <116511558+Vlorethaqi@users.noreply.github.com>
2025-10-16 23:27:07 +0200

    Restored file to original state

--- app/App.jsx
+++ app/App.jsx
@@ -1,11 +1,28 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to TravelMate 🌍</Text>
-      <Link href="/details">Go to Destination Details</Link>
-    </View>
-  );
+export default function HomeScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}>Welcome to TravelMate on Index 🌍</Text>
+            <Link style={styles.text} href={"/App"}>Go to App</Link>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#fff",
+        backgroundColor: "#000"
+    },
+    text: {
+        color: "#fff",
+    }
+});
+
+
+
