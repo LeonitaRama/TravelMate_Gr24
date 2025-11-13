a9b852fc36d2c310dba023e5adf3653601a72aba
rinesa-bejic <rinesa.bejic@student.uni-pr.edu>
2025-11-13 13:54:14 +0100

    fixed an  error

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,15 +1,17 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGjNeCD4-5vbb5pul3qgmCsVmT4OhlMAo",
+  apiKey: "AIzaSyDGjNECD4-5vbbb5pul3gqmcSvmT4OhUMA0",
   authDomain: "myreactnativeapp-def5a.firebaseapp.com",
   projectId: "myreactnativeapp-def5a",
-  storageBucket: "myreactnativeapp-def5a.firebasestorage.app",
+  storageBucket: "myreactnativeapp-def5a.appspot.com",
   messagingSenderId: "519217992001",
   appId: "1:519217992001:web:0b999d48921c54bcb5d1c0",
-  measurementId: "G-4JNW53JVGL"
+  measurementId: "G-4JNW53JVGL",
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); 
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app)
