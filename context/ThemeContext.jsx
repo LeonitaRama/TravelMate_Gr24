811a0c1dfbd1420a641ec9b477ca9f48aa724080
Rona Kukaj <ronakukaj@gmail.com>
2025-11-14 15:12:37 +0100

    added Async Storage for dark mode

--- context/ThemeContext.jsx
+++ context/ThemeContext.jsx
@@ -1,9 +1,23 @@
-import React, { createContext, useState } from "react";
-
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
+
+ useEffect(() => {
+    (async () => {
+      const savedTheme = await AsyncStorage.getItem("@theme");
+      if (savedTheme !== null) {
+        setDarkMode(savedTheme === "dark");
+      }
+    })();
+  }, []);
+
+  const toggleTheme = async (value) => {
+    setDarkMode(value);
+    await AsyncStorage.setItem("@theme", value ? "dark" : "light");
+  };
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
