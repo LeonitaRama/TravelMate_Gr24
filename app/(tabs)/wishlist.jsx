427fa51412d079e5c6c272fb6ba3c350a1a69928
Vlore Thaqi <116511558+Vlorethaqi@users.noreply.github.com>
2025-11-15 16:24:55 +0100

    UI fixes

--- app/(tabs)/wishlist.jsx
+++ app/(tabs)/wishlist.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, Text, FlatList, Image, StyleSheet } from "react-native";
+import { View, Text, FlatList, Image, StyleSheet ,TouchableOpacity} from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ThemeContext } from "../../context/ThemeContext";
 import { lightTheme, darkTheme } from "../../context/ThemeStyles";
+import { Ionicons } from "@expo/vector-icons"; 
 
 export default function WishlistScreen() {
   const [favorites, setFavorites] = useState([]);
@@ -28,23 +29,41 @@ function fixImageSource(image) {
       }
     };
     loadFavorites();
+
   }, []);
+  const removeFavorite = async (id) => {
+    const updated = favorites.filter((item) => item.id !== id);
+    setFavorites(updated);
+    await AsyncStorage.setItem("@favorites", JSON.stringify(updated));
+  };
+
 
   const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Image source={fixImageSource(item.image)} style={{ width: 150, height: 150 }} />
+    <View style={[styles.card, { backgroundColor: theme.card }]}>
+<Image source={fixImageSource(item.image)} style={styles.image }/>    
+   <View style={styles.info}> 
+
       <Text style={[styles.name, {color:theme.text}]}>{item.name}</Text>
       <Text style={[styles.desc, {color:theme.textSecondary}]}>{item.desc}</Text>
+
       {item.review ? <Text style={[styles.review, {color:"green"}]}>{item.review}</Text> : null}
+    </View>
+     <TouchableOpacity 
+        style={styles.deleteBtn} 
+        onPress={() => removeFavorite(item.id)}
+      >
+        <Ionicons name="trash" size={22} color="white" />
+      </TouchableOpacity>
     </View>
   );
 
+  
   return (
     
     <View style={[styles.container, { backgroundColor: theme.background }]}>
       <Text style={[styles.title, { color: theme.text }]}>❤️ Wishlist</Text>
       {favorites.length === 0 ? (
-        <Text style={[styles.subtitle, { color: theme.text }]}>Your saved trips will appear here.</Text>
+      <Text style={[styles.empty, { color: theme.textSecondary }]}> No saved destinations yet.</Text>
   
       ) : (
         <FlatList
@@ -64,10 +83,15 @@ function fixImageSource(image) {
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20},
-  title: { fontSize: 24, fontWeight: "bold", marginBottom: 15 },
-  subtitle: { fontSize: 16 },
-  item: { marginBottom: 20, alignItems: "center" },
-  name: { fontWeight: "bold", fontSize: 16, marginTop: 5 },
-  desc: { fontStyle: "italic", textAlign: "center", marginTop: 5, width: 200 },
-  review: { marginTop: 5, fontWeight: "bold" },
+  title: { fontSize: 28, fontWeight: "bold", marginBottom: 15 },
+  empty: { fontSize: 18,marginTop:20,textAlign: "center"},
+   card: { flexDirection: "row",padding: 12,borderRadius: 16, marginBottom: 16,shadowColor: "#000",shadowOpacity: 0.15,           
+    shadowRadius: 6,elevation: 5,alignItems: "center",position: "relative"
+  },
+ image: {  width: 110, height: 110,borderRadius: 14,marginRight: 12 }, 
+  name: { fontWeight: "bold", fontSize: 18 },
+  desc: {marginTop: 4,fontSize: 14,fontStyle: "italic",width: "90%" },
+  review: { marginTop: 6, fontSize: 14, fontWeight: "600"},
+  deleteBtn: {position: "absolute",right: 12,bottom: 12,backgroundColor: "red",padding: 8,borderRadius: 10 }
+
 });
