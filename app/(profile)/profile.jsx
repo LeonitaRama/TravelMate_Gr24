a8625c76f6d5039b21e47f71d9a11012db91610b
Leonita Rama <leonita.rama1@student.uni-pr.edu>
2025-11-16 15:53:21 +0100

    added confirm modal for profile page to add photo

--- app/(profile)/profile.jsx
+++ app/(profile)/profile.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { Image, StyleSheet, Text, View, ActivityIndicator, TouchableOpacity, ScrollView } from 'react-native'
+import { Image, StyleSheet, Text, View, ActivityIndicator, TouchableOpacity, ScrollView, StatusBar } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRouter } from "expo-router";
 import ProfileOption from './ProfileOption';
@@ -8,10 +8,12 @@ import { auth1 as auth } from "../../firebase/firebaseConfig"
 import { ThemeContext } from '../../context/ThemeContext';
 import { lightTheme, darkTheme } from '../../context/ThemeStyles';
 import { signOut } from 'firebase/auth';
-
+import ConfirmModal from '../(components)/ConfirmModal';
 
 const Profile = () => {
     const [user, setUser] = useState(null);
+    const [showLogoutModal, setShowLogoutModal] = useState(false);
+    const [showPhotoOptions, setShowPhotoOptions] = useState(false);
     const router = useRouter();
     const { darkMode } = useContext(ThemeContext);
     const theme = darkMode ? darkTheme : lightTheme;
@@ -50,19 +52,21 @@ const Profile = () => {
 
     return (
         <SafeAreaView style={[styles.container, { backgroundColor: theme.background }]}>
+            <StatusBar barStyle={darkMode ? "light-content" : "dark-content"} />
             <ScrollView contentContainerStyle={[styles.scroll, { backgroundColor: theme.background }]}>
-                <View style={[styles.header, { backgroundColor: theme.header }]}>
-
+                <View style={[styles.header, { backgroundColor: theme.icon }]}>
                     <View style={styles.imageContainer}>
                         <Image
                             source={{ uri: user.photoURL || 'https://loremfaces.net/96/id/.jpg' }}
                             style={styles.image}
                         />
+                        
                         <FontAwesome6
                             name="add"
                             size={24}
                             color="black"
                             style={styles.addIcon}
+                            onPress={() => setShowPhotoOptions(true)}
                         />
                     </View>
                 </View>
@@ -76,12 +80,33 @@ const Profile = () => {
                 <ProfileOption title="Reviews" iconName="star-outline" headerTitle="Reviews" target="/(profile)/reviews" />
                 <ProfileOption title="Wishlist" iconName="heart-outline" headerTitle="Wishlist" target="/(tabs)/wishlist" />
                 <ProfileOption title="Photos" iconName="images-outline" headerTitle="Photos" target="/(profile)/photos" />
-            </ScrollView>
 
-            <TouchableOpacity style={[styles.logoutButton, { backgroundColor: theme.icon }]} onPress={handleLogout}
+            <TouchableOpacity style={[styles.logoutButton, { backgroundColor: theme.icon }]} onPress={() => setShowLogoutModal(true)}
                 activeOpacity={0.6} >
                 <Text style={styles.logoutBtnText}>Log Out</Text>
             </TouchableOpacity>
+                </ScrollView>
+<ConfirmModal
+    visible={showPhotoOptions}
+    title="Profile Photo"
+    onClose={() => setShowPhotoOptions(false)}
+    buttons={[
+        { label: "Take Photo", color: "#6b63ff", onPress: () => console.log("camera here") },
+        { label: "Choose from Gallery", color: "#6b63ff", onPress: () => console.log("gallery here") },
+        { label: "Remove Photo", color: "#d9534f", onPress: () => console.log("delete here") },
+        { label: "Cancel", color: "gray" }
+    ]}
+/>
+<ConfirmModal
+    visible={showLogoutModal}
+    title="Log Out"
+    message="A jeni i sigurt që doni të dilni?"
+    onClose={() => setShowLogoutModal(false)}
+    buttons={[
+        { label: "Dil", color: "#d9544f7b", onPress: handleLogout },
+        { label: "Anulo", color: "gray" }
+    ]}
+/>
         </SafeAreaView>
     )
 }
@@ -107,7 +132,7 @@ const styles = StyleSheet.create({
     },
     headerTop: {
         flexDirection: 'row',
-        backgroundColor: "#6b63ff73",
+        backgroundColor: "#cc0f0f",
         alignItems: 'center',
         gap: 10,
     },
@@ -142,17 +167,22 @@ const styles = StyleSheet.create({
         elevation: 4
     },
     addIcon: {
-        position: 'absolute',
-        bottom: 5,
-        right: 5,
-        backgroundColor: '#fff',
-        borderRadius: 15,
-        padding: 5,
-        boxShadow: "4px 0 0 4 5",
-        elevation: 5,
+     position: 'absolute',
+    bottom: 5,
+    right: 5,
+    backgroundColor: '#fff',
+    borderColor: '#000',
+    borderWidth: 3,     
+    borderRadius: 15,
+    padding: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
     },
     button: {
-        backgroundColor: "#6b63ff",
+        backgroundColor: "#6b63ff0",
         padding: 14,
         borderRadius: 10,
         alignItems: "center",
@@ -181,7 +211,6 @@ const styles = StyleSheet.create({
         width: "350",
         alignItems: 'center',
         borderRadius: 10,
-        elevation: 6,
         padding: 25,
         paddingHorizontal: 100,
         shadowColor: "#000",
