b6093ab6cdd225136ca661c3cfa20e41e86c8148
Liridona Kurrumeli <kurrumelil@gmail.com>
2025-12-21 19:37:21 +0100

    .

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -19,7 +19,6 @@ import {
 } from 'react-native';
 import WhyChooseUs from "../(components)/WhyChooseUs.jsx";
 import { useTranslation } from 'react-i18next';
-import { useRouter } from 'expo-router';
 
 
 
